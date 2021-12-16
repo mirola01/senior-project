601cc341101927866c81327c08c2f1a0d26435ef
curiouspaul1 <170408025@live.unilag.edu.ng>
2021-12-16 22:26:51 +0100

    update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-// const { join } = require("path");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 // Endpoint to serve the configuration file
 app.get("/auth_config.json", (req, res) => {
