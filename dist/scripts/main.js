7a856281849035f7a7b76625b1b5b4da16cf1600
mirola01 <mirola01@luther.edu>
2023-10-05 10:54:14 -0500

    Update main.js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,7 +3,18 @@ import * as Database from './database.js';
 import * as UI from './ui.js';
 import * as Events from './events.js';
 
-window.onload = async () => {
+
+const add_nw_btn = document.querySelector('#add-new-btn');
+const body_ = document.querySelector('.container');
+const form = document.querySelector('.form');
+
+add_nw_btn.addEventListener('click', (e) => {
+  console.log(e)
+  body_.classList.add('container-fade');
+  form.style.display = 'block';
+})
+
+document.querySelector('#auth-btn').addEventListener('click', async (e) => {
   let auth0 = null;
   let isAuthenticated = false;
   auth0 = await Auth.configureClient();
@@ -21,20 +32,7 @@ window.onload = async () => {
   });
   const data = await response.json();
   console.log('API Response:', data.message);
-
-};
-
-
-const add_nw_btn = document.querySelector('#add-new-btn');
-const body_ = document.querySelector('.container');
-const form = document.querySelector('.form');
-
-add_nw_btn.addEventListener('click', (e) => {
-  console.log(e)
-  body_.classList.add('container-fade');
-  form.style.display = 'block';
-})
-
+});
 document.querySelector('#close-btn').addEventListener('click', (e) => {
   console.log(e)
   body_.classList.remove('container-fade');
