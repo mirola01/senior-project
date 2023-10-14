7aa5738629b8b97b88b16bf60263534a2aeb4bdc
mirola01 <mirola01@luther.edu>
2023-10-14 18:30:16 -0500

    Update main.js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -8,34 +8,32 @@ let auth0 = null;
 let isAuthenticated = false;
 
 // Initialize the Auth0 client when the window loads
-window.addEventListener('load', async () => {
-    try {
-      await Auth.configureClient();
-    } catch (err) {
-      console.error('Failed to initialize Auth0 client:', err);
-      return;
-    }
-
-  // You can access the auth0 instance if needed
-  auth0 = Auth.getAuth0();
-  console.log("load", auth0);
-  // Debugging code
-  setTimeout(async () => {
-    const debugIsAuthenticated = await auth0.isAuthenticated();
-    console.log('Debug Auth?', debugIsAuthenticated);
-    console.log('Debug Error:', error);
-    console.log('Auth0 Transaction:', auth0.transaction);
-    console.log('Auth0 Scope:', auth0.options.scope);
-  }, 2000);
-
+window.onload = async () => {
+  let auth0 = null;
+  let isAuthenticated = false;
+  auth0 = await Auth.configureClient();
   isAuthenticated = await auth0.isAuthenticated();
-  console.log("Auth?", isAuthenticated);
-});
+  console.log('why:', isAuthenticated);
+  UI.updateUI(auth0);
+
+};
+
+
+// UI elements
+const add_nw_btn = document.querySelector('#add-new-btn');
+add_nw_btn.setAttribute('aria-label', 'Add new Button');
+
+const body_ = document.querySelector('.container');
+const form = document.querySelector('.form');
+form.setAttribute('tabindex', '-1'); // to manage focus
 
 // Handle authentication button click
 document.querySelector('#auth-btn').addEventListener('click', async (e) => {
   console.log("auth click");
-  if (document.querySelector('#auth-btn').innerHTML === 'Login') {
+  const authBtn = document.querySelector('#auth-btn');
+  authBtn.setAttribute('aria-label', authBtn.innerHTML === 'Login' ? 'Login Button' : 'Logout Button');
+
+  if (authBtn.innerHTML === 'Login') {
     console.log("login");
     Auth.login();
   } else {
@@ -43,21 +41,16 @@ document.querySelector('#auth-btn').addEventListener('click', async (e) => {
   }
 });
 
-
-// UI elements
-const add_nw_btn = document.querySelector('#add-new-btn');
-const body_ = document.querySelector('.container');
-const form = document.querySelector('.form');
-
 add_nw_btn.addEventListener('click', (e) => {
   console.log(e)
   body_.classList.add('container-fade');
+  // Manage focus when form is displayed
   form.style.display = 'block';
+  form.focus();
 });
 
 document.querySelector('#close-btn').addEventListener('click', (e) => {
   console.log(e)
   body_.classList.remove('container-fade');
   form.style.display = 'none';
-
 });
