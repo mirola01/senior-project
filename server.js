2a1e53205abd4333df10e574f9d0aa17e534aeb6
mirola01 <mirola01@luther.edu>
2023-10-14 18:42:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+// Import required modules
 const express = require('express');
-const cors = require('cors');
+const { auth, requiresAuth } = require('express-openid-connect');
 
+// Initialize Express app
 const app = express();
 
-app.use(cors());
+// Auth0 Configuration
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'https://lineup-manager.netlify.app', // Your actual base URL
+  clientID: 'Xe6t07ETgQBkSvipbSCCFRbxaBmeDMEC', // Your Auth0 Client ID
+  issuerBaseURL: 'https://dev-n84gx3uanib6ojpf.us.auth0.com', // Your Auth0 Domain
+  secret: process.env.clientSecret
+};
 
+// Use Auth0 authentication
+app.use(auth(config));
+
+// Define routes
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+// Start server
+app.listen(3000, function() {
+  console.log('Listening on http://localhost:3000');
 });
