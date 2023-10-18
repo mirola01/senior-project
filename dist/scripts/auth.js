71c7bfe09aa22838454d46b17ec6374675117648
mirola01 <mirola01@luther.edu>
2023-10-18 11:49:35 -0500

    Update auth.js

--- dist/scripts/auth.js
+++ dist/scripts/auth.js
@@ -19,7 +19,7 @@ export const configureClient = async () => {
         });
         console.log("2 --> Auth", auth0)
         isAuthenticated = await auth0.isAuthenticated();
-        yourAuth0Token = await auth0.getTokenSilently();
+        let yourAuth0Token = await auth0.getTokenSilently();
         console.log('2 --> Auth?', yourAuth0Token);
     } catch (e) {
         console.error('Auth0 client initialization failed:', e);
