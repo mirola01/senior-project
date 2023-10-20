2edc952a2958bd541261daa0f5202b37c4c6d738
mirola01 <mirola01@luther.edu>
2023-10-20 08:33:13 -0500

    Update auth_config.js

--- netlify/functions/auth_config.js
+++ netlify/functions/auth_config.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      domain: DOMAIN,
+      domain: "https://dev-n84gx3uanib6ojpf.us.auth0.com",
       client_id: clientID,
       audience: aud
     })
