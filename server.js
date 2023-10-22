4b44b2199bb31da87f84081993594fa5edf1a5da
mirola01 <mirola01@luther.edu>
2023-10-22 16:47:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Import required modules
 require("dotenv").config();
-
 const express = require("express");
 const { auth, requiresAuth } = require("express-openid-connect");
+const cors = require('cors');  // <-- Add this line
 
 // Initialize Express app
 const app = express();
@@ -11,19 +11,19 @@ const app = express();
 const config = {
   authRequired: false,
   auth0Logout: true,
-  baseURL: "https://lineup-manager.netlify.app/", 
-  clientID: process.env.clientID, // Your Auth0 Client ID
-  issuerBaseURL: process.env.DOMAIN, // Your Auth0 Domain
+  baseURL: "https://lineup-manager.netlify.app/",
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.DOMAIN,
   secret: process.env.clientSecret,
 };
 
 // Use Auth0 authentication
 app.use(auth(config));
 
+// Add CORS middleware  <-- Add this block
 app.use(cors({
   origin: 'https://lineup-manager.netlify.app'
 }));
-
 
 // Define routes
 app.get("/", (req, res) => {
