27bae281c3b9631fadc0eff78caae2fe57db8d25
mirola01 <90211920+mirola01@users.noreply.github.com>
2023-10-22 13:55:00 -0500

    Update main.js

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,8 +1,8 @@
 // Import modules
-import * as Auth from './auth.js';
-import * as Database from './database.js';
-import * as UI from './ui.js';
-import * as Events from './events.js';
+import * as Auth from "./auth.js";
+import * as Database from "./database.js";
+import * as UI from "./ui.js";
+import * as Events from "./events.js";
 
 let auth0Client = null;
 // Initialize the Auth0 client when the window loads
@@ -10,40 +10,45 @@ window.onload = async () => {
   try {
     await Auth.configureClient();
     const auth0 = Auth.getAuth0();
+
     const query = window.location.search;
-  if (query.includes("code=") && query.includes("state=")) {
+    if (query.includes("code=") && query.includes("state=")) {
+      // Process the login state
+      await auth0.handleRedirectCallback();
 
-    // Process the login state
-    await auth0.handleRedirectCallback();
-    
-    console.log("Authentificated")
+      console.log("Authentificated");
 
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, "/player-database.html");}
+      // Use replaceState to redirect the user away and remove the querystring parameters
+      window.history.replaceState({}, document.title, "/player-database.html");
+    }
+
     const isAuthenticated = await auth0.isAuthenticated();
-    console.log('Auth?', isAuthenticated);
+    console.log("Auth?", isAuthenticated);
     UI.updateUI(auth0);
   } catch (e) {
-    console.error('Initialization failed:', e);
+    console.error("Initialization failed:", e);
   }
   //UI.setupTabs();
 };
 
 // UI elements
-const add_nw_btn = document.querySelector('#add-new-btn');
-add_nw_btn.setAttribute('aria-label', 'Add new Button');
+const add_nw_btn = document.querySelector("#add-new-btn");
+add_nw_btn.setAttribute("aria-label", "Add new Button");
 
-const body_ = document.querySelector('.container');
-const form = document.querySelector('.form');
-form.setAttribute('tabindex', '-1'); // to manage focus
+const body_ = document.querySelector(".container");
+const form = document.querySelector(".form");
+form.setAttribute("tabindex", "-1"); // to manage focus
 
 // Handle authentication button click
-document.querySelector('#auth-btn').addEventListener('click', async (e) => {
+document.querySelector("#auth-btn").addEventListener("click", async (e) => {
   console.log("auth click");
-  const authBtn = document.querySelector('#auth-btn');
-  authBtn.setAttribute('aria-label', authBtn.innerHTML === 'Login' ? 'Login Button' : 'Logout Button');
+  const authBtn = document.querySelector("#auth-btn");
+  authBtn.setAttribute(
+    "aria-label",
+    authBtn.innerHTML === "Login" ? "Login Button" : "Logout Button"
+  );
 
-  if (authBtn.innerHTML === 'Login') {
+  if (authBtn.innerHTML === "Login") {
     console.log("login");
     Auth.login();
   } else {
@@ -51,16 +56,17 @@ document.querySelector('#auth-btn').addEventListener('click', async (e) => {
   }
 });
 
-add_nw_btn.addEventListener('click', (e) => {
+add_nw_btn.addEventListener("click", (e) => {
   console.log(e);
-  body_.classList.add('container-fade');
+  body_.classList.add("container-fade");
   // Manage focus when form is displayed
-  form.style.display = 'block';
+  form.style.display = "block";
   form.focus();
 });
 
-document.querySelector('#close-btn').addEventListener('click', (e) => {
+document.querySelector("#close-btn").addEventListener("click", (e) => {
   console.log(e);
-  body_.classList.remove('container-fade');
-  form.style.display = 'none';
+  body_.classList.remove("container-fade");
+  form.style.display = "none";
 });
+
