c1a463f1b82ff68fc9eedcb37e8ae924f7599161
mirola01 <mirola01@luther.edu>
2023-10-23 08:27:32 -0500

    Update ui.js

--- dist/scripts/ui.js
+++ dist/scripts/ui.js
@@ -23,15 +23,15 @@ export async function updateUI() {
         /**
          * Check the role
          */
-        console.log("current token", q.CurrentToken())
-        let token = await client.query(
-            q.CurrentToken()
-        );
-        console.log("token", token);
-        let user_role = token["https:/db.fauna.com/roles"][0];
-        console.log("user_role",user_role);
+        // console.log("current token", q.CurrentToken())
+        // let token = await client.query(
+        //     q.CurrentToken()
+        // );
+        // console.log("token", token);
+        // let user_role = token["https:/db.fauna.com/roles"][0];
+        // console.log("user_role",user_role);
 
-        if (user_role === 'user') {
+        // if (user_role === 'user') {
             document.querySelector('#add-new-btn').style.display = 'block';
 
             let players = await client.query(
@@ -78,38 +78,38 @@ export async function updateUI() {
             //players_section.innerHTML += htmlText.join('');
             //players_section.appendChild(addCard)
             //document.querySelector('.please-login').style.display = 'none';
-        } else {
-            document.querySelector('#add-new-btn').style.display = 'none';
-            document.querySelector('#new-card-btn').style.display = 'none';
-            document.querySelector('#search-bar').style.display = 'none';
-            document.querySelector('.admin-msg').style.display = 'block';
-            //players_section = document.querySelector('.card-container');
-            let players = await client.query(
-                q.Map(
-                    q.Paginate(
-                        q.Match(q.Index('allPlayers'))
-                    ),
-                    q.Lambda(
-                        "X", q.Get(q.Var("X"))
-                    )
-                )
+        // } else {
+        //     document.querySelector('#add-new-btn').style.display = 'none';
+        //     document.querySelector('#new-card-btn').style.display = 'none';
+        //     document.querySelector('#search-bar').style.display = 'none';
+        //     document.querySelector('.admin-msg').style.display = 'block';
+        //     //players_section = document.querySelector('.card-container');
+        //     let players = await client.query(
+        //         q.Map(
+        //             q.Paginate(
+        //                 q.Match(q.Index('allPlayers'))
+        //             ),
+        //             q.Lambda(
+        //                 "X", q.Get(q.Var("X"))
+        //             )
+        //         )
 
-            );
+        //     );
 
-            //players_section.style.display = 'grid';
-            const tableBody = document.querySelector('#player-table tbody');
-            var htmlText = players['data'].map(function (o) {
-                return `
-                    <tr>
-                    <td>${o.data.name}</td>
-                    <td>${o.data.age}</td>
-                    <td>${o.data.position}</td>
-                    </tr>`;
-            });
-            tableBody.innerHTML = htmlText.join('');
+        //     //players_section.style.display = 'grid';
+        //     const tableBody = document.querySelector('#player-table tbody');
+        //     var htmlText = players['data'].map(function (o) {
+        //         return `
+        //             <tr>
+        //             <td>${o.data.name}</td>
+        //             <td>${o.data.age}</td>
+        //             <td>${o.data.position}</td>
+        //             </tr>`;
+        //     });
+        //     tableBody.innerHTML = htmlText.join('');
 
-            // document.querySelector('.please-login').style.display = 'None';
-        }
+        //     // document.querySelector('.please-login').style.display = 'None';
+        // }
 
     } else {
         console.log("not auth")
