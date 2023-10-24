abe0529b9daf10a172a6d394d3da73f98b06f240
mirola01 <mirola01@luther.edu>
2023-10-24 13:18:08 -0500

    Update database.js

--- dist/scripts/database.js
+++ dist/scripts/database.js
@@ -46,7 +46,7 @@ export const new_player = async () => {
             name: document.querySelector('#playerName').value,
             age: document.querySelector('#playerAge').value,
             position: document.querySelector('#playerPosition').value,
-            owner: q.CurrentIdentity()
+            owner: token
           }
         })
       ).then((ret) => console.log(ret))
