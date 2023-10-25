287d4fe11c44528a493ecf9ea937e824abf52555
mirola01 <mirola01@luther.edu>
2023-10-25 08:08:21 -0500

    .

--- netlify/functions/players_by_owner.js
+++ netlify/functions/players_by_owner.js
@@ -3,6 +3,7 @@ const faunadb = require("faunadb");
 const q = faunadb.query;
 
 exports.handler = async function (event, context) {
+  const { userId } = JSON.parse(event.body);
   const _client = new faunadb.Client({
     secret: process.env.fauna_key,
     domain: "db.us.fauna.com",
@@ -13,10 +14,10 @@ exports.handler = async function (event, context) {
   try {
     let allPlayers = await _client.query(
       q.Map(
-        q.Paginate(q.Match(q.Index("players_by_owner"), event.body.userId)),
+        q.Paginate(q.Match(q.Index("players_by_owner"), userId)),
         q.Lambda("X", q.Get(q.Var("X")))
       ),
-      { owner: event.body.userId }
+      { secret: event.body.token }
     );
 
     if (!allPlayers) {
