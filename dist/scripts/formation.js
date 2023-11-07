564ce92cd5b9ac889a20dbf9f9686e31ffb5b2a9
mirola01 <mirola01@luther.edu>
2023-11-07 14:26:14 -0600

    Update formation.js

--- dist/scripts/formation.js
+++ dist/scripts/formation.js
@@ -1,4 +1,3 @@
-// Presuming this is the entire script for the formation.html
 import * as Auth from "./auth.js";
 var faunadb = window.faunadb;
 var q = faunadb.query;
@@ -29,7 +28,7 @@ async function loadFormationFromFaunaDB() {
         displayFormations(formations);
       });
       //wc_team.updateFormation(response.data); // Update the team formation with the data
-      renderPositions(); // Then render the positions
+      renderPlayers(); // Then render the positions
     } catch (error) {
       console.error('Error fetching formation:', error);
     }
@@ -65,9 +64,211 @@ function displayFormations(formationData) {
     document.getElementById('starting_11').className = formationName;
     renderPositions(players); // Call renderPositions here with the actual formation data
   }
-
-  
 }
+const wc_team = {}; // Initialize wc_team if it doesn't exist
+
+wc_team.dragDrap = (function() {
+  
+  let position;
+  let dragSrc;
+  
+  const init = () => {
+    console.log("Init called");
+    const players = document.querySelectorAll(".positions ul");
+  
+    players.forEach(player => {
+      player.addEventListener("dragstart", dragStart, true);
+      player.addEventListener("dragend", dragEnd, false);
+    });
+  
+    position = document.querySelectorAll('#starting_11 li');
+  
+    position.forEach(pos => {
+      pos.addEventListener('drop', drop, false);
+      pos.addEventListener('dragenter', cancel, false);
+      pos.addEventListener('dragover', cancel, false);
+    });
+  };
+  
+  const dragStart = (e) => {
+    dragSrc = e;
+    e.dataTransfer.setData("text/html", e.target.outerHTML);
+    e.dataTransfer.effectAllowed = "copy";
+    e.dataTransfer.dropEffect = "copy";
+  
+    position.forEach(pos => {
+      if (e.currentTarget.dataset.pos === pos.dataset.pos) {
+        pos.classList.add("highlight");
+      }
+    });
+  };
+  
+  const dragStartOver = (e) => {
+    dragSrc = e;
+    e.dataTransfer.setData("text/html", e.target.innerHTML);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.dropEffect = "move";
+  
+    position.forEach(pos => {
+      if (e.currentTarget.dataset.pos === pos.dataset.pos) {
+        pos.classList.add("highlight");
+      }
+    });
+  };
+  
+  const drop = (e) => {
+    cancel(e);
+  
+    const data = e.dataTransfer.getData("text/html");
+    const target = e.currentTarget;
+    target.style.opacity = 1;
+    if (target.innerHTML !== "") {
+      dragReset(target.lastChild.dataset.player);
+    }
+  
+    if (dragSrc.target.parentNode.id === 'starting_11') {
+      const dragSrcClass = dragSrc.target.className;
+      const targetClass = target.className;
+  
+      dragSrc.target.innerHTML = target.innerHTML;
+      dragSrc.target.className = targetClass;
+  
+      target.innerHTML = data;
+      target.className = dragSrcClass;
+    } else {
+      target.innerHTML = '<a class="remove"></a>' + data;
+      target.classList.add('selected');
+      dragSrc.target.draggable = false;
+      target.lastChild.setAttribute('draggable', 'false');
+    }
+  
+    dragEnd();
+    target.addEventListener("dragstart", dragStartOver, false);
+  };
+  
+  const dragEnd = () => {
+    position.forEach(pos => {
+      pos.classList.remove('highlight');
+    });
+  };
+  
+  const cancel = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  };
+  
+  const dragReset = (name) => {
+    const player = document.querySelector(`.positions div[data-player="${name}"]`);
+    player.draggable = true;
+  };
+  
+  return {
+    init
+  };
+})();
+
+async function renderPlayers() {
+  console.log("renderPositions triggered");
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    const decodedJWT = jwt_decode(accessToken);
+
+    const fauna_key = Auth.getFaunaKey();
+    var client = new faunadb.Client({
+      secret: fauna_key,
+      domain: "db.us.fauna.com",
+      port: 443,
+      scheme: "https",
+    });
+
+    let token = await client.query(q.CurrentToken());
+    token = token.value.id;
+
+    let players = await fetch("/.netlify/functions/players_by_owner", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ token, userId: decodedJWT["sub"] }),
+    });
+    players = await players.json();
+
+    const positions = { Goalkeeper: [], Defender: [], Midfield: [], Forward: [] };
+
+    players.data.forEach((player) => {
+      const pos = player.data.position;
+      if (positions[pos]) {
+        positions[pos].push(player);
+      }
+    });
+    console.log(positions)
+    const sectionElement = document.querySelector('section');
+    Object.keys(positions).forEach((key) => {
+      const div = document.createElement('div');
+      div.className = 'positions menu';
+      div.innerHTML = `<a>${key.toUpperCase()}</a>`;
+
+      const ul = document.createElement('ul');
+      ul.setAttribute('data-pos', key);
+
+      positions[key].forEach((player) => {
+        console.log("key", key, positions[key]);
+        const li = document.createElement('li');
+        const ul2 = document.createElement('ul');
+        const divPlayer = document.createElement('div');
+        divPlayer.setAttribute('draggable', 'true');
+        divPlayer.setAttribute('data-player', player.data.name);
+
+        const img = document.createElement('img');
+        img.setAttribute('draggable', 'false');
+        const jerseyNumber = player.data.jersey;
+        const playerImage = player.data.imageURL || generateDefaultImage(jerseyNumber);
+        img.setAttribute('src', playerImage);
+
+        const p = document.createElement('p');
+        p.textContent = player.data.name;
+        ul2.style.display = "flex";
+        ul2.style.flexDirection = "column";
+        li.style.alignItems = "center";
+
+        divPlayer.appendChild(img);
+        li.appendChild(divPlayer);
+        ul2.appendChild(li);
+        ul2.appendChild(p);
+        ul.appendChild(ul2);
+      });
+
+      div.appendChild(ul);
+      sectionElement.appendChild(div);
+    });
+    wc_team.dragDrap.init();
+  }
+}
+  function generateDefaultImage(jerseyNumber) {
+    const canvas = document.createElement('canvas');
+    canvas.width = 100;
+    canvas.height = 100;
+    
+    const ctx = canvas.getContext('2d');
+    
+    // Draw background
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    // Draw text
+    ctx.fillStyle = 'white';
+    ctx.font = '48px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    console.log("Jersey", jerseyNumber)
+    ctx.fillText(jerseyNumber, canvas.width / 2, canvas.height / 2);
+    
+    // Convert to data URL
+    const dataURL = canvas.toDataURL('image/png');
+    
+    return dataURL;
+  }
 
 
 function renderPositions(formationData) {
@@ -83,14 +284,16 @@ function renderPositions(formationData) {
     // Iterate over each player in the position list
     playerList.forEach((playerName, index) => {
       if (playerName !== 'NO_PLAYER') {
-        console.log("playerName", playerName)
-        const playerElement = document.createElement('div');
-        playerElement.textContent = playerName; // Here you would add your player's image and styling
-        playerElement.classList.add('player', position);
-        playerElement.setAttribute('data-index', index);
-        // Append to the respective position container on the field
-        document.querySelector(`.${position}-container`).appendChild(playerElement);
-      }
+        const player = getPlayerByName(playerName);
+        if (player) {
+          // Create image element for the player using player.data.jersey
+          const playerImageElement = createPlayerImageElement(player.data.jersey, player.data.imageURL);
+          // Append this image to the corresponding position on the field
+          document.getElementById(`pos${index}`).appendChild(playerImageElement);
+          // Set draggable to false since this player is already in the formation
+          playerImageElement.draggable = false;
+
+      }}
     });
   }
   // After rendering the positions, initialize any drag-and-drop functionality
@@ -138,20 +341,3 @@ function updateFormation(formation) {
     forwards.appendChild(li);
   }
 }  
-
-
-
-function clearField() {
-  // Remove existing players from the field
-  document.querySelectorAll('.player').forEach(player => player.remove());
-}
-
-function setupFormationSelector(wc_team) {
-  document.getElementById('formationSelector').addEventListener('change', function () {
-    const selectedFormation = this.value;
-    document.getElementById('starting_11').className = selectedFormation;
-    wc_team.updateFormation(selectedFormation);
-    wc_team.makePlayersDraggable();
-    wc_team.dragDrap.init();
-  });
-}
