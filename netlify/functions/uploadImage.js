9bc631d91f49aca214f459db8ad48ad0a997f803
mirola01 <mirola01@luther.edu>
2023-11-13 19:12:41 -0600

    Update uploadImage.js

--- netlify/functions/uploadImage.js
+++ netlify/functions/uploadImage.js
@@ -2,13 +2,12 @@ require("dotenv").config();
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET
-});
-
 exports.handler = async (event) => {
     try {
+        const s3 = new AWS.S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY,
+            secretAccessKey: process.env.AWS_SECRET
+        });
         const body = JSON.parse(event.body);
         const base64String = body.image;
         const buffer = Buffer.from(base64String, 'base64');
