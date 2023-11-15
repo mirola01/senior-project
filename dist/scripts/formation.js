15d32ace92a9ea2ebc17a37e0b93c119532742f8
mirola01 <mirola01@luther.edu>
2023-11-15 15:44:06 -0600

    Update formation.js

--- dist/scripts/formation.js
+++ dist/scripts/formation.js
@@ -17,23 +17,28 @@ const client = new faunadb.Client({
   port: 443,
   scheme: "https",
 });
-let positionsObject = { Goalkeeper: [], Defender: [], Midfield: [], Forward: [] };
+let positionsObject = {
+  Goalkeeper: [],
+  Defender: [],
+  Midfield: [],
+  Forward: []
+};
 let formationId;
 /**
  * Loads a specific formation from FaunaDB on DOMContentLoaded event.
  * Attaches event listeners for formation selection and player drag-and-drop functionality.
  */
 document.addEventListener("DOMContentLoaded", function () {
-  loadFormationFromFaunaDB(); 
+  loadFormationFromFaunaDB();
 
-  document.getElementById('formationSelector').addEventListener('change', function() {
+  document.getElementById('formationSelector').addEventListener('change', function () {
     const selectedFormation = this.value;
     document.getElementById('starting_11').className = selectedFormation;
     updateFormation(selectedFormation);
     makePlayersDraggable();
     wc_team.dragDrap.init();
   });
-  
+
 });
 
 async function loadFormationFromFaunaDB() {
@@ -43,7 +48,7 @@ async function loadFormationFromFaunaDB() {
     try {
       let token = await client.query(q.CurrentToken());
       token = token.value.id;
-      renderPlayers(); 
+      renderPlayers();
       fetchFormation(token, formationId).then(formations => { // Pass the token here
         displayFormations(formations);
       });
@@ -89,7 +94,7 @@ function displayFormations(formationData) {
   // Find the option with the matching value
   const matchingOption = formationSelector.querySelector(`option[value="${formationName}"]`);
   if (matchingOption) {
-    updateFormation(formationName); 
+    updateFormation(formationName);
     matchingOption.selected = true;
     document.getElementById('starting_11').className = formationName;
     renderPositions(players); // Call renderPositions here with the actual formation data
@@ -101,72 +106,72 @@ const wc_team = {}; // Initialize wc_team if it doesn't exist
  * A namespace for handling drag-and-drop functionality within the team's formation.
  * @namespace
  */
-wc_team.dragDrap = (function() {
-  
+wc_team.dragDrap = (function () {
+
   let position;
   let dragSrc;
-  
+
   const init = () => {
     console.log("Init called");
     const players = document.querySelectorAll(".positions ul");
-  
+
     players.forEach(player => {
       player.addEventListener("dragstart", dragStart, true);
       player.addEventListener("dragend", dragEnd, false);
     });
-  
+
     position = document.querySelectorAll('#starting_11 li');
-  
+
     position.forEach(pos => {
       pos.addEventListener('drop', drop, false);
       pos.addEventListener('dragenter', cancel, false);
       pos.addEventListener('dragover', cancel, false);
     });
   };
-  
+
   const dragStart = (e) => {
     dragSrc = e;
     e.dataTransfer.setData("text/html", e.target.outerHTML);
     e.dataTransfer.effectAllowed = "copy";
     e.dataTransfer.dropEffect = "copy";
-  
+
     position.forEach(pos => {
       if (e.currentTarget.dataset.pos === pos.dataset.pos) {
         pos.classList.add("highlight");
       }
     });
   };
-  
+
   const dragStartOver = (e) => {
     dragSrc = e;
     e.dataTransfer.setData("text/html", e.target.innerHTML);
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.dropEffect = "move";
-  
+
     position.forEach(pos => {
       if (e.currentTarget.dataset.pos === pos.dataset.pos) {
         pos.classList.add("highlight");
       }
     });
   };
-  
+
   const drop = (e) => {
     cancel(e);
-  
+
     const data = e.dataTransfer.getData("text/html");
     const target = e.currentTarget;
     target.style.opacity = 1;
     if (target.innerHTML !== "") {
       dragReset(target.lastChild.dataset.player);
     }
-  
+
     if (dragSrc.target.parentNode.id === 'starting_11') {
       const dragSrcClass = dragSrc.target.className;
       const targetClass = target.className;
-  
+
       dragSrc.target.innerHTML = target.innerHTML;
       dragSrc.target.className = targetClass;
-  
+
       target.innerHTML = data;
       target.className = dragSrcClass;
     } else {
@@ -175,28 +180,28 @@ wc_team.dragDrap = (function() {
       dragSrc.target.draggable = false;
       target.lastChild.setAttribute('draggable', 'false');
     }
-  
+
     dragEnd();
     target.addEventListener("dragstart", dragStartOver, false);
   };
-  
+
   const dragEnd = () => {
     position.forEach(pos => {
       pos.classList.remove('highlight');
     });
   };
-  
+
   const cancel = (e) => {
     e.preventDefault();
     e.stopPropagation();
     return false;
   };
-  
+
   const dragReset = (name) => {
     const player = document.querySelector(`.positions div[data-player="${name}"]`);
     player.draggable = true;
   };
-  
+
   return {
     init
   };
@@ -227,7 +232,10 @@ async function renderPlayers() {
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify({ token, userId: decodedJWT["sub"] }),
+      body: JSON.stringify({
+        token,
+        userId: decodedJWT["sub"]
+      }),
     });
     players = await players.json();
 
@@ -285,102 +293,104 @@ async function renderPlayers() {
  * @param {number} jerseyNumber - The jersey number to display on the default image.
  * @returns {string} - A data URL for the generated image.
  */
-  function generateDefaultImage(jerseyNumber) {
-    const canvas = document.createElement('canvas');
-    canvas.width = 100;
-    canvas.height = 100;
-    
-    const ctx = canvas.getContext('2d');
-    
-    // Draw background
-    ctx.fillStyle = 'gray';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    // Draw text
-    ctx.fillStyle = 'white';
-    ctx.font = '48px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    console.log("Jersey", jerseyNumber)
-    ctx.fillText(jerseyNumber, canvas.width / 2, canvas.height / 2);
-    
-    // Convert to data URL
-    const dataURL = canvas.toDataURL('image/png');
-    
-    return dataURL;
-  }
+function generateDefaultImage(jerseyNumber) {
+  const canvas = document.createElement('canvas');
+  canvas.width = 100;
+  canvas.height = 100;
+
+  const ctx = canvas.getContext('2d');
+
+  // Draw background
+  ctx.fillStyle = 'gray';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Draw text
+  ctx.fillStyle = 'white';
+  ctx.font = '48px Arial';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  console.log("Jersey", jerseyNumber)
+  ctx.fillText(jerseyNumber, canvas.width / 2, canvas.height / 2);
+
+  // Convert to data URL
+  const dataURL = canvas.toDataURL('image/png');
+
+  return dataURL;
+}
 
 /**
  * Displays the player positions within a specific formation.
  * 
  * @param {Object} formationData - The data object containing players and their positions.
  */
-  function renderPositions(formationData) {
-    Object.keys(formationData).forEach((positionGroup) => {
-      const positionList = document.querySelector(`.${positionGroup}`);
-      const playerNames = formationData[positionGroup];
-      playerNames.forEach((playerName, index) => {
-        const position = positionList.children[index];
-        if (playerName !== 'NO_PLAYER') {
-          console.log("renderPositions", playerName);
-          const player = getPlayerByName(playerName);
-          if (player) {
-            const playerElement = createPlayerElement(player.data);
-            position.innerHTML = ''; // Clear any existing content
-            position.appendChild(playerElement);
-            position.className = "selected";
-            position.style.opacity = 1;
-          }
-        } else {
-          // Clear any existing content if the player is "NO_PLAYER"
-          position.innerHTML = '';
-          position.draggable = true;
+function renderPositions(formationData) {
+  Object.keys(formationData).forEach((positionGroup) => {
+    const positionList = document.querySelector(`.${positionGroup}`);
+    const playerNames = formationData[positionGroup];
+    playerNames.forEach((playerName, index) => {
+      const position = positionList.children[index];
+      if (playerName !== 'NO_PLAYER') {
+        console.log("renderPositions", playerName);
+        const player = getPlayerByName(playerName);
+        if (player) {
+          const playerElement = createPlayerElement(player.data);
+          position.innerHTML = ''; // Clear any existing content
+          position.appendChild(playerElement);
+          position.className = "selected";
+          position.style.opacity = 1;
         }
-      });
+      } else {
+        // Clear any existing content if the player is "NO_PLAYER"
+        position.innerHTML = '';
+        position.draggable = true;
+      }
     });
-  }
-  /**
+  });
+}
+/**
  * Creates an HTML element for a player with the provided data.
  * 
  * @param {Object} playerData - The data object containing the player's details.
  * @returns {HTMLElement} - The constructed player HTML element.
  */
-  function createPlayerElement(playerData) {
-    const divPlayer = document.createElement('div');
-    divPlayer.setAttribute('data-player', playerData.name);
-    divPlayer.draggable = false; // Player in the formation should not be draggable
-    console.log("playerData", playerData)
-    const img = document.createElement('img');
-    img.src = generateDefaultImage(playerData.jersey);
-    img.alt = playerData.name;
-    
-    divPlayer.appendChild(img);    
-    return divPlayer;
-  }
-  /**
+function createPlayerElement(playerData) {
+  const divPlayer = document.createElement('div');
+  divPlayer.setAttribute('data-player', playerData.name);
+  divPlayer.draggable = false; // Player in the formation should not be draggable
+  console.log("playerData", playerData)
+  const img = document.createElement('img');
+  img.src = generateDefaultImage(playerData.jersey);
+  img.alt = playerData.name;
+
+  divPlayer.appendChild(img);
+  return divPlayer;
+}
+/**
  * Retrieves player information by name from the positions object.
  * 
  * @param {string} playerName - The name of the player to retrieve.
  * @returns {Object|null} - The player object if found, otherwise null.
  */
-  function getPlayerByName(playerName) {
-    // Iterate over each position group in player_info
-    for (const positionGroup in positionsObject) {
-      console.log("positionGroup", positionGroup)
-      console.log("positionsObject", positionsObject)
-      if (positionsObject.hasOwnProperty(positionGroup)) {
-        // Find the player in the current position group
-        const player = positionsObject[positionGroup].find(p => p.data.name === playerName);
-        console.log("player in getPlayerByName", player)
-        if (player) {
-          return player;
-        }
-      }
+function getPlayerByName(playerName) {
+  const playerNameNormalized = playerName.trim().toLowerCase();
+  console.log("Normalized playerName:", playerNameNormalized);
+
+  for (const group in positionsObject) {
+    const player = positionsObject[group].find(p => {
+      const playerNameInObject = p.data.name.trim().toLowerCase();
+      console.log(`Comparing with: ${playerNameInObject}`);
+      return playerNameInObject === playerNameNormalized;
+    });
+
+    if (player) {
+      console.log("Found player:", player);
+      return player;
     }
-    // Return null if the player is not found in any position group
-    return null;
   }
-  /**
+
+  console.log("Player not found");
+}
+/**
  * Updates the UI to reflect a new formation structure when a different formation is selected.
  * 
  * @param {string} formation - The selected formation structure, e.g., "4-4-2".
@@ -431,65 +441,65 @@ function updateFormation(formation) {
 /**
  * Updates the current lineup in FaunaDB with the changes made on the UI.
  */
-  async function updateLineup() {
-    const accessToken = localStorage.getItem('accessToken');
-    if (accessToken) {
-      const decodedJWT = jwt_decode(accessToken);
-      const fauna_key = Auth.getFaunaKey();
-      var client = new faunadb.Client({
-        secret: fauna_key,
-        domain: 'db.us.fauna.com',
-        port: 443,
-        scheme: 'https'
-      });
-  
-      // Get selected formation from the dropdown
-  const selectedFormation = document.querySelector('#formationSelector').value;
+async function updateLineup() {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) {
+    const decodedJWT = jwt_decode(accessToken);
+    const fauna_key = Auth.getFaunaKey();
+    var client = new faunadb.Client({
+      secret: fauna_key,
+      domain: 'db.us.fauna.com',
+      port: 443,
+      scheme: 'https'
+    });
 
-  // Initialize object to hold player positions based on the selected formation
-  let playersInFormation = {};
-  
-  if (selectedFormation === 'formation-4-4-2') {
-    playersInFormation = {
-      "gk": ["NO_PLAYER"],
-      "df": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
-      "md": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
-      "fw": ["NO_PLAYER", "NO_PLAYER"]
-    };
-  } else if (selectedFormation === 'formation-4-3-3') {
-    playersInFormation = {
-      "gk": ["NO_PLAYER"],
-      "df": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
-      "md": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
-      "fw": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER"]
-    };
-  } else if (selectedFormation === 'formation-3-5-2') {
-    playersInFormation = {
-      "gk": ["NO_PLAYER"],
-      "df": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
-      "md": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
-      "fw": ["NO_PLAYER", "NO_PLAYER"]
-    };
-  }
-  try {
+    // Get selected formation from the dropdown
+    const selectedFormation = document.querySelector('#formationSelector').value;
+
+    // Initialize object to hold player positions based on the selected formation
+    let playersInFormation = {};
+
+    if (selectedFormation === 'formation-4-4-2') {
+      playersInFormation = {
+        "gk": ["NO_PLAYER"],
+        "df": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
+        "md": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
+        "fw": ["NO_PLAYER", "NO_PLAYER"]
+      };
+    } else if (selectedFormation === 'formation-4-3-3') {
+      playersInFormation = {
+        "gk": ["NO_PLAYER"],
+        "df": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
+        "md": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
+        "fw": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER"]
+      };
+    } else if (selectedFormation === 'formation-3-5-2') {
+      playersInFormation = {
+        "gk": ["NO_PLAYER"],
+        "df": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
+        "md": ["NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER", "NO_PLAYER"],
+        "fw": ["NO_PLAYER", "NO_PLAYER"]
+      };
+    }
+    try {
       // Extract players from the HTML
       const playerElements = document.querySelectorAll('li');
       playerElements.forEach((element) => {
         const position = element.parentNode.className; // Getting the class of the parent <ul>, which should indicate the position (gk, df, md, fw)
         const index = Array.from(element.parentNode.children).indexOf(element); // Getting the index of the li inside its parent ul
-        const playerName = element.querySelector('div') ? element.querySelector('div').getAttribute('data-player') : "NO_PLAYER"; 
+        const playerName = element.querySelector('div') ? element.querySelector('div').getAttribute('data-player') : "NO_PLAYER";
         if (playersInFormation[position]) {
-            playersInFormation[position][index] = playerName;
+          playersInFormation[position][index] = playerName;
         }
       });
       console.log("Formation", playersInFormation)
-  
+
       let data = await client.query(
         q.Update(q.Ref(q.Collection('Formation'), formationId), {
           data: {
             name: document.querySelector('.titleFormation').textContent,
             formation: selectedFormation,
-            players: playersInFormation,  // Saving the players in each position
+            players: playersInFormation, // Saving the players in each position
             owner: decodedJWT['sub']
           }
         })
@@ -530,9 +540,8 @@ async function clearLineup() {
 
   makePlayersDraggable();
   wc_team.dragDrap.init();
-}    
+}
 
 // Event listeners for the 'Save Lineup' and 'Clear Lineup' buttons.
 document.querySelector('.update-lineup').addEventListener('click', updateLineup);
 document.querySelector('.clear-lineup').addEventListener('click', clearLineup);
-
