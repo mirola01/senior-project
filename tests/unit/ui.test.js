4f235d752fb3fbb40f22d926f35ea0f6a5ee7db7
mirola01 <mirola01@luther.edu>
2023-11-30 20:53:53 -0600

    .

--- tests/unit/ui.test.js
+++ tests/unit/ui.test.js
@@ -1,59 +1,74 @@
-const { updateUI } = require('./ui');
-const { getFaunaKey } = require('./auth.js');
+const { chromium } = require('playwright');
+const faunadb = require('faunadb');
+const q = faunadb.query;
 
-// Mocking local storage
-const localStorageMock = (function() {
-  let store = {};
+jest.mock('faunadb', () => {
+  const originalModule = jest.requireActual('faunadb');
+
   return {
-    getItem: jest.fn((key) => store[key] || null),
-    setItem: jest.fn((key, value) => {
-      store[key] = value.toString();
-    }),
-    clear: jest.fn(() => {
-      store = {};
-    })
+    ...originalModule,
+    query: {
+      ...originalModule.query,
+      Create: jest.fn(),
+      Delete: jest.fn(),
+      // Add other methods as necessary
+    },
   };
-})();
-
-// Mocking auth.js
-jest.mock('./auth.js', () => ({
-  getFaunaKey: jest.fn()
-}));
-
-// Mocking fetch
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ data: [{ /* mock player data */ }] })
-  })
-);
-
-Object.defineProperty(window, 'localStorage', {
-  value: localStorageMock
 });
 
-describe('UI Tests for Player Database', () => {
-  beforeEach(() => {
-    // Reset mocks before each test
-    jest.clearAllMocks();
-    document.body.innerHTML = `<div id="playersContainer"></div>`;
+describe('Player Database Page', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await chromium.launch();
   });
 
-  it('should update UI for an authenticated user', async () => {
-    // Set up mock localStorage for authenticated user
-    localStorageMock.getItem.mockImplementation((key) => {
-      if (key === 'accessToken') return 'mockedAccessToken';
-      return null;
-    });
-
-    getFaunaKey.mockReturnValue('mockedFaunaKey');
-    
-    await updateUI();
-
-    // Assertions to check if players are fetched and UI is updated accordingly
-    expect(localStorageMock.getItem).toHaveBeenCalledWith('accessToken');
-    expect(fetch).toHaveBeenCalledWith(expect.any(String), expect.any(Object));
-    // More assertions based on how your UI is expected to update
+  afterAll(async () => {
+    await browser.close();
   });
 
-  // More tests for different scenarios...
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto('https://lineup-manager.netlify.app/player-database.html'); // Replace with the correct path
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  test('Adding a new player', async () => {
+    // Mocking the Create function to resolve with provided data
+    faunadb.query.Create.mockImplementation((...args) => Promise.resolve({ data: args[0] }));
+
+    // Simulate adding a new player through the UI
+    await page.click('#add-new-btn');
+    await page.fill('#playerName', 'John Doe');
+    await page.fill('#playerPosition', 'Forward');
+    await page.click('.submit');
+
+    // Verify if the Create function was called correctly
+    expect(faunadb.query.Create).toHaveBeenCalled();
+
+    // Check if a new player is added to the list in the UI
+    const newPlayer = await page.$('.player[name="John Doe"]');
+    expect(newPlayer).toBeTruthy();
+  });
+
+  test('Deleting a player', async () => {
+    // Mocking the Delete function to resolve with provided data
+    faunadb.query.Delete.mockImplementation((ref) => Promise.resolve({ ref }));
+
+    // Assuming there's a delete button for each player
+    await page.click('.player:first-child .delete');
+
+    // Verify if the Delete function was called correctly
+    expect(faunadb.query.Delete).toHaveBeenCalled();
+
+    // Check if the player is removed from the UI
+    const deletedPlayer = await page.$('.player:first-child');
+    expect(deletedPlayer).toBeNull();
+  });
+
+  // Additional test cases...
 });
