ccacb458a00436ac42c782360da0fc9294b67a29
mirola01 <mirola01@luther.edu>
2023-12-06 18:33:49 -0600

    .

--- tests/e2e/guide.e2e.js
+++ tests/e2e/guide.e2e.js
@@ -43,16 +43,14 @@
       }, 30000); // Optional increased timeout
     
       test('Tab interaction and content visibility', async () => {
-        const tabIds = ['f-4-4-2', 'f-4-3-3', 'f-3-5-2'];
+        const tabIds = ['4-4-2', '4-3-3', '3-5-2'];
         for (const tabId of tabIds) {
-          await page.click(`#${tabId}`);
+          await page.click(`#f-${tabId}`);
           await page.waitForTimeout(500);
           const isTabVisible = await page.evaluate((id) => {
-            const tabContent = document.querySelector(`#${id}`);
-            console.log("tab visible " + id); // Print debug information in the browser's console
+            const tabContent = document.querySelector(`#f-${id}`);
             return tabContent && getComputedStyle(tabContent).display !== 'none';
           }, tabId);
-          console.log("tab visible " + tabId + isTabVisible); // Print debug information in the Node.js console
           expect(isTabVisible).toBeTruthy();
         }
       }, 30000);
